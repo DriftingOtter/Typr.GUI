9faf84538eb5a634818e85767d3bb0ed335b350f
Otter <dakshkaul7790@gmail.com>
2023-01-11 00:13:21 +0530

    removed old gui file. make func for acc,wpm,tk

--- TGB_Git/IA - Typing go burrr/CLI_Init_Dev/Typing_Go_Burr_CLI.py
+++ TGB_Git/IA - Typing go burrr/CLI_Init_Dev/Typing_Go_Burr_CLI.py
@@ -1,23 +1,42 @@
-# ------------------------------------------------------------------------------#
-# Name: Typing Go Burrr (Command Line Version)
-# Version: 1.0.0
-# Summary: A Minimalistic Typing Test Application In The Terminal
-# Home-page: N/A
-# Author: Daksh Kaul
-# Author-email: dakshkaul7790@gmail.com
-# License: N/A
-# ------------------------------------------------------------------------------#
-
 # Imports
 import random
 import string
-import time 
+import time
+
+
+def TextAcc(plyr_text, displayTextSTR, word_count):
+
+    # Calculates Text Acc
+    textACC = len(set(plyr_text.split()) & set(displayTextSTR.split()))
+    textACC = (textACC / word_count) * 100
+
+    return textACC
+
+
+def TimeTaken(time_STOP, time_START):
+
+    # Gather Time Taken
+    time_taken = int(time_STOP - time_START)
+
+    return time_taken
+
+
+def WordsPerMinute(time_STOP, time_START, word_count, timeTaken):
+
+    # Calculates Time Taken
+    timeTaken = time_STOP - time_START
+
+    # Calculates Words Per Minute
+    wordsPM = int((word_count / timeTaken) * 100)
+
+    return wordsPM
+
 
 # Holds Word List Location
-worldList = 'C:/Users/daksh/OneDrive/Desktop/TypingGOBurrr/TGB_Git/IA - Typing go burrr/Word List/Loki_Word_List_EN.txt'
+worldList = "TGB_Git/IA - Typing go burrr/CLI_Init_Dev/Loki_Word_List_EN.txt"
 
-# Reads Word List Into A Variable 
-currenText = open(worldList,"r")
+# Reads Word List Into A Variable
+currenText = open(worldList, "r")
 
 # Reads The Line Number From Text
 lines = currenText.readlines()
@@ -26,31 +45,31 @@ lines = currenText.readlines()
 displayText = []
 
 # makes loop for adding words into the displayText VAR
-for i in range (0,10):
+for i in range(0, 10):
     if i != 10:
-        randomLineGen = random.randint(0, 977)
+        randomLineGen = random.randint(0, 977)  # the number of words in the word list
         displayText.append(lines[randomLineGen])
     else:
         break
 
 # Removes  default array boiler plate
-displayText = ''.join([str(elem) for elem in displayText])
+displayText = "".join([str(elem) for elem in displayText])
 
-# Displays Text without white spaces 
-print(displayText.translate({ord(c): ' ' for c in string.whitespace}))
+# Displays Text without white spaces
+print(displayText.translate({ord(c): " " for c in string.whitespace}))
 print("")
 
 # Stores display data into a string only var
-displayTextSTR = displayText.translate({ord(c): ' ' for c in string.whitespace})
+displayTextSTR = displayText.translate({ord(c): " " for c in string.whitespace})
 
-# Finds Word Length Of Text 
+# Finds Word Length Of Text
 word_count = len(displayTextSTR.split())
 
 # Records Start Time
 time_START = time.time()
 
 # Allows User Input
-plyr_text = input(">> ")
+plyr_text = input("// ")
 
 # Records Stop Time
 time_STOP = time.time()
@@ -61,42 +80,18 @@ if plyr_text.strip() == displayTextSTR.strip():
     # Print Result Message
     print("\nYou Did It ! Wanna Do Another ?\n")
 
-    # Calculates Text Acc
-    textACC = len(set(plyr_text.split()) & set(displayTextSTR.split()))
-    textACC = ((textACC/word_count)*100)
-
-    # Calculates Time Taken
-    timeTaken = time_STOP - time_START
-    wordPM = int((word_count/timeTaken)*100)
-
-    # Gather Time Taken
-    timer_counter_val = int(time_STOP - time_START)
-
-    # Displays The WPM and ACC
-    print("Words Per Minute:", wordPM, "\n")
-    print("Accuracy:", textACC, "%\n")
-    print("Time Taken:", timer_counter_val, "s\n")
+    print("Accuracy:", TextAcc(plyr_text, displayTextSTR, word_count))
+    print("Time Taken:", TimeTaken(time_STOP, time_START))
+    print("Words Per Minute:", WordsPerMinute(time_STOP, time_START, word_count, TimeTaken(time_STOP, time_START)))
 
 else:
 
     # Print Result Message
     print("\nNice Try! Some Mistakes There Though, Wanna Do Another ?\n")
 
-    # Calculates Text Acc
-    textACC = len(set(plyr_text.split()) & set(displayTextSTR.split()))
-    textACC = ((textACC/word_count)*100)
-
-    # Calculates Time Taken
-    timeTaken = time_STOP - time_START
-    wordPM = int((word_count/timeTaken)*100)
-
-    # Gather Time Taken
-    timer_counter_val = int(time_STOP - time_START)
-
-    # Displays The WPM and ACC
-    print("Words Per Minute:", wordPM, "\n")
-    print("Accuracy:", textACC, "%\n")
-    print("Time Taken:", timer_counter_val, "s\n")
+    print("Accuracy:", TextAcc(plyr_text, displayTextSTR, word_count))
+    print("Time Taken:", TimeTaken(time_STOP, time_START))
+    print("Words Per Minute:", WordsPerMinute(time_STOP, time_START, word_count, TimeTaken(time_STOP, time_START)))
 
 # Closes Main File After Use
 currenText.close()
