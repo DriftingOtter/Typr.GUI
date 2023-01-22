f55996b89d5bbf8e94775bcaf2ab835df3644e04
Otter <dakshkaul7790@gmail.com>
2023-01-22 12:39:07 +0530

    made activehiglighting func

--- TyprGUI/main.py
+++ TyprGUI/main.py
@@ -5,13 +5,13 @@ import string
 import time
 
 
-def refactorSpecialChar(text):
+# def refactorSpecialChar(text):
 
-    global specialChar_mapping
+#     global specialChar_mapping
 
-    # Look up the corresponding character in the char_mapping dictionary
-    # If the text is not in the mapping, use the original text
-    return specialChar_mapping.get(text, text)
+#     # Look up the corresponding character in the char_mapping dictionary
+#     # If the text is not in the mapping, use the original text
+#     return specialChar_mapping.get(text, text)
 
 def TextAcc(plyr_text, displayText, word_count):
 
@@ -41,25 +41,25 @@ def WordsPerMinute(time_STOP, time_START, word_count, timeTaken):
     return wordsPM
 
 
-def key_press(event):
+# def key_press(event):
 
-    for row in (key_row1, key_row2, key_row3, key_row4, key_row5):
-        for btn in row.winfo_children():
-            if isinstance(btn, Button):
-                if (str(btn["text"]).strip() in (event.char, event.keysym)) or (
-                    str(btn["text"]).lower().strip() in (event.char, event.keysym)
-                ):
-                    btn["relief"] = "sunken"
+#     for row in (key_row1, key_row2, key_row3, key_row4, key_row5):
+#         for btn in row.winfo_children():
+#             if isinstance(btn, Button):
+#                 if (str(btn["text"]).strip() in (event.char, event.keysym)) or (
+#                     str(btn["text"]).lower().strip() in (event.char, event.keysym)
+#                 ):
+#                     btn["relief"] = "sunken"
 
-def key_release(event):
+# def key_release(event):
 
-    for row in (key_row1, key_row2, key_row3, key_row4, key_row5):
-        for btn in row.winfo_children():
-            if isinstance(btn, Button):
-                if (str(btn["text"]).strip() in (event.char, event.keysym)) or (
-                    str(btn["text"]).lower().strip() in (event.char, event.keysym)
-                ):
-                    btn["relief"] = "raised"
+#     for row in (key_row1, key_row2, key_row3, key_row4, key_row5):
+#         for btn in row.winfo_children():
+#             if isinstance(btn, Button):
+#                 if (str(btn["text"]).strip() in (event.char, event.keysym)) or (
+#                     str(btn["text"]).lower().strip() in (event.char, event.keysym)
+#                 ):
+#                     btn["relief"] = "raised"
 
 def countdown(timercount):
 
@@ -92,10 +92,44 @@ def startcountdown():
 
     countdown(10)
 
+intcounter = 0
+def check_letter(event):
+
+    global internalText, usrEntryBox, intcounter
+
+    print(intcounter)
+
+    if not internalText or not usrEntryBox:
+
+        raise ValueError("internalText or usrEntryBox is not defined!")
+
+    last_letter = usrEntryBox.get("end-2c", "end-1c")
+    print(last_letter)
+
+    correct_letter = internalText[intcounter]
+    print(correct_letter)
+
+    usrEntryBox.tag_config("ErrorColor", background='red')
+
+    # TODO:- FIX HOW TEXT ERROR TAG NOT WORKING
+
+    if last_letter != correct_letter:
+
+        usrEntryBox.tag_add("ErrorColor", "end-2c", "end-1c")
+
+    elif last_letter == correct_letter:
+
+        intcounter = intcounter + 1
+        usrEntryBox.tag_remove("ErrorColor", "1.0", "end-1c")
+
+    else:
+
+        pass
+
 #------------------------------------------------------------------------------------------
 
 # Holds Word List Location
-worldList = "TyprGUI/Loki_Word_List_EN.txt"
+worldList = "C:/Users/daksh/OneDrive/Desktop/Typr/TyprGUI/Loki_Word_List_EN.txt"
 
 with open(worldList, "r") as currenText:
     # Reads The Line Number From Text
@@ -180,155 +214,155 @@ usrEntryBox = Text(
 )
 usrEntryBox.pack(anchor=CENTER)
 
-# Makes frame that acts as a 'case' for the key to be displayed in
-keyboardCase = Frame(
-    master=root, 
-    borderwidth=40, 
-    bg="#050505", 
-    height=125, 
-    width=250, 
-    relief=GROOVE
-)
-# keyboardCase.pack()
+# # Makes frame that acts as a 'case' for the key to be displayed in
+# keyboardCase = Frame(
+#     master=root, 
+#     borderwidth=40, 
+#     bg="#050505", 
+#     height=125, 
+#     width=250, 
+#     relief=GROOVE
+# )
+# # keyboardCase.pack()
 
-# indivisual rows for key to be placed in
-key_row1 = Frame(
-    master=keyboardCase,
-    width=125,
-    takefocus=0
-)
-key_row1.pack(side=TOP)
+# # indivisual rows for key to be placed in
+# key_row1 = Frame(
+#     master=keyboardCase,
+#     width=125,
+#     takefocus=0
+# )
+# key_row1.pack(side=TOP)
 
-key_row2 = Frame(
-    master=keyboardCase,
-    width=125,
-    takefocus=0
-)
-key_row2.pack(anchor=CENTER)
+# key_row2 = Frame(
+#     master=keyboardCase,
+#     width=125,
+#     takefocus=0
+# )
+# key_row2.pack(anchor=CENTER)
 
-key_row3 = Frame(
-    master=keyboardCase,
-    width=125,
-    takefocus=0
-)
-key_row3.pack(anchor=CENTER)
+# key_row3 = Frame(
+#     master=keyboardCase,
+#     width=125,
+#     takefocus=0
+# )
+# key_row3.pack(anchor=CENTER)
 
-key_row4 = Frame(
-    master=keyboardCase,
-    width=125,
-    takefocus=0
-)
-key_row4.pack(anchor=CENTER)
+# key_row4 = Frame(
+#     master=keyboardCase,
+#     width=125,
+#     takefocus=0
+# )
+# key_row4.pack(anchor=CENTER)
 
-key_row5 = Frame(
-    master=keyboardCase,
-    width=125,
-    takefocus=0
-)
-key_row5.pack(side=BOTTOM)
+# key_row5 = Frame(
+#     master=keyboardCase,
+#     width=125,
+#     takefocus=0
+# )
+# key_row5.pack(side=BOTTOM)
 
 
-# Create a list of keys to display on the keyboard
-keys_row1 = [
-    "    `    ",
-    "    1    ",
-    "    2    ",
-    "    3    ",
-    "    4    ",
-    "    5    ",
-    "    6    ",
-    "    7    ",
-    "    8    ",
-    "    9    ",
-    "    0    ",
-    "    -    ",
-    "    =    ",
-    "             BackSpace               ",
-]
+# # Create a list of keys to display on the keyboard
+# keys_row1 = [
+#     "    `    ",
+#     "    1    ",
+#     "    2    ",
+#     "    3    ",
+#     "    4    ",
+#     "    5    ",
+#     "    6    ",
+#     "    7    ",
+#     "    8    ",
+#     "    9    ",
+#     "    0    ",
+#     "    -    ",
+#     "    =    ",
+#     "             BackSpace               ",
+# ]
 
-keys_row2 = [
-    "                        Tab                     ",
-    "    Q    ",
-    "    W    ",
-    "    E    ",
-    "    R    ",
-    "    T    ",
-    "    Y    ",
-    "    U    ",
-    "    I    ",
-    "    O    ",
-    "    P    ",
-    "    [    ",
-    "    ]    ",
-    "   \\    ",
-]
+# keys_row2 = [
+#     "                        Tab                     ",
+#     "    Q    ",
+#     "    W    ",
+#     "    E    ",
+#     "    R    ",
+#     "    T    ",
+#     "    Y    ",
+#     "    U    ",
+#     "    I    ",
+#     "    O    ",
+#     "    P    ",
+#     "    [    ",
+#     "    ]    ",
+#     "   \\    ",
+# ]
 
-keys_row3 = [
-    "       Caps_Lock       ",
-    "    A    ",
-    "    S    ",
-    "    D    ",
-    "    F    ",
-    "    G    ",
-    "    H    ",
-    "    J    ",
-    "    K    ",
-    "    L    ",
-    "    ;    ",
-    '    "    ',
-    "                Return              ",
-]
+# keys_row3 = [
+#     "       Caps_Lock       ",
+#     "    A    ",
+#     "    S    ",
+#     "    D    ",
+#     "    F    ",
+#     "    G    ",
+#     "    H    ",
+#     "    J    ",
+#     "    K    ",
+#     "    L    ",
+#     "    ;    ",
+#     '    "    ',
+#     "                Return              ",
+# ]
 
-keys_row4 = [
-    "                    Shift_L                  ",
-    "    Z    ",
-    "    X    ",
-    "    C    ",
-    "    V    ",
-    "    B    ",
-    "    N    ",
-    "    M    ",
-    "    ,    ",
-    "    .    ",
-    "    /    ",
-    "                  Shift_R                 ",
-]
+# keys_row4 = [
+#     "                    Shift_L                  ",
+#     "    Z    ",
+#     "    X    ",
+#     "    C    ",
+#     "    V    ",
+#     "    B    ",
+#     "    N    ",
+#     "    M    ",
+#     "    ,    ",
+#     "    .    ",
+#     "    /    ",
+#     "                  Shift_R                 ",
+# ]
 
-keys_row5 = [
-    "   Control_L   ",
-    "    Win_L  ",
-    "  Alt_L    ",
-    "                                                         Space                                                       ",
-    "  Alt_R    ",
-    "Left",
-    "Up",
-    "Down",
-    "Right",
-]
+# keys_row5 = [
+#     "   Control_L   ",
+#     "    Win_L  ",
+#     "  Alt_L    ",
+#     "                                                         Space                                                       ",
+#     "  Alt_R    ",
+#     "Left",
+#     "Up",
+#     "Down",
+#     "Right",
+# ]
 
-rows = (keys_row1, keys_row2, keys_row3, keys_row4, keys_row5)
+# rows = (keys_row1, keys_row2, keys_row3, keys_row4, keys_row5)
 
-for i, key_row in enumerate((key_row1, key_row2, key_row3, key_row4, key_row5)):
-    for key in rows[i]:
-        btn = Button(
-            key_row,
-            text=key,
-            border=10,
-            relief=RAISED,
-            padx=5,
-            height=3,
-            font=("Rubik Bold", 10),
-            bg="#c4c1b9",
-            takefocus=0
-        )
-        btn.pack(side="left")
+# for i, key_row in enumerate((key_row1, key_row2, key_row3, key_row4, key_row5)):
+#     for key in rows[i]:
+#         btn = Button(
+#             key_row,
+#             text=key,
+#             border=10,
+#             relief=RAISED,
+#             padx=5,
+#             height=3,
+#             font=("Rubik Bold", 10),
+#             bg="#c4c1b9",
+#             takefocus=0
+#         )
+#         btn.pack(side="left")
 
 # Binding For Applications
-root.bind("<Key>", key_press)
-root.bind("<KeyRelease>", key_release)
+#root.bind("<Key>", key_press)
+#root.bind("<KeyRelease>", key_release)
 root.bind("<Configure>", lambda event: gameInputAndOutputFrame.place_configure(relx=.5, rely=.5, anchor="center"))
 
 usrEntryBox.bind("<KeyPress>", is_typing)
-
+usrEntryBox.bind("<KeyRelease>", check_letter)
 if __name__ == "__main__":
     root.mainloop()
