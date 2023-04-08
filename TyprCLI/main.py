d70ee9b200cad1d16fa2028d4a519636d0c43d0b
Ott6r@Linux <dakshkaul7790@gmail.com>
2023-04-09 01:17:33 +0530

    Full Revamp Via Rich Module, && Error checking for invalid tests.

--- TyprCLI/main.py
+++ TyprCLI/main.py
@@ -1,55 +1,48 @@
+#======================
 # Imports
+#======================
 import sys
 import random
 import string
 import time
 import os
-from colorama import Fore, Back, Style
-from colorama import init
 
-# Auto resets styles after each print
-init(autoreset=True)
+from rich.traceback import install
+from rich import print 
+from rich.panel import Panel
+from rich.console import Group
+from rich.console import Console
 
+
+
+#==========================
+# Enables Console From Rich
+#==========================
+console = Console()
+
+
+
+#=======================
+# Enables Rich Traceback
+#=======================
+install()
+
+
+
+#============================
+# Applicaton Global Variables
+#============================
 time_START = float()
 time_STOP = float()
-word_count = int()
 internalText = []
 displayText = []
-tryAgain = str()
-
-# Holds Word List Location
 wordList = "/home/otter/Documents/Typr/WordLists/Loki_Word_List_EN.txt"
 
 
-# Functions For Calculation
-def TextAcc(plyr_text, displayText, word_count):
 
-    # Calculates Text Acc
-    textACC = len(set(plyr_text.split()) & set(displayText.split()))
-    textACC = (textACC / word_count) * 100
-
-    return int(textACC)
-
-
-def TimeTaken(time_STOP, time_START):
-
-    # Gather Time Taken
-    time_taken = int(time_STOP - time_START)
-
-    return time_taken
-
-
-def WordsPerMinute(time_STOP, time_START, word_count, timeTaken):
-
-    # Calculates Time Taken
-    timeTaken = time_STOP - time_START
-
-    # Calculates Words Per Minute
-    wordsPM = int((word_count / timeTaken) * 100)
-
-    return wordsPM
-
-
+#==========================
+# Text Generation Functions
+#==========================
 def generateChallengeText():
 
     global wordList, displayText, internalText, word_count
@@ -84,12 +77,27 @@ def generateChallengeText():
     displayText = displayText.translate({ord(c): " " for c in string.whitespace})
 
 
-    # Finds Word Length Of Text
-    word_count = len(displayText.split())
 
-    print(Fore.RED + "[", displayText, Fore.RED + "]")
+#======================
+# Display Text Function 
+#======================
+def displayChallengeText():
+
+    global displayText
+
+    print(
+        Panel(
+            displayText,
+
+            title="[bold italic]Typr[/]",
+            )
+        )
 
 
+
+#====================
+# User Input Function
+#====================
 def plyrStart():
 
     global time_START, time_STOP, plyr_text
@@ -97,43 +105,117 @@ def plyrStart():
     # Records Start Time
     time_START = time.time()
 
-    # Allows User Input
-    plyr_text = input(Fore.CYAN + "> ")
+    # Records Users Input
+    try:
+        plyr_text = console.input("[bold blue]> [/]")
+    except:
+        pass
 
     # Records Stop Time
     time_STOP = time.time()
 
 
-def checkPlyrScore():
 
-    global plyr_text, displayText, word_count, time_STOP, time_START, word_count
+#======================
+# Calculation Functions
+#======================
+def textAcc(plyr_text, displayText, word_count):
 
-    # Checks If User Input Is Same As Printed Text
-    if plyr_text.strip() == displayText.strip():
+    # Calculates Text Acc
+    textACC = len(set(plyr_text.split()) & set(displayText.split()))
 
-        # Print Result Message
-        print(Fore.GREEN + "\nYou Did It ! Wanna Do Another ?\n")
+    textACC = int((textACC / word_count) * 100)
 
-        print(Fore.BLUE + "Accuracy:", TextAcc(plyr_text, displayText, word_count), "%")
-        print(Fore.YELLOW + "Time Taken:", TimeTaken(time_STOP, time_START), "s")
-        print(Fore.MAGENTA + "Words Per Minute:", 
-              WordsPerMinute(time_STOP, time_START, word_count, TimeTaken(time_STOP, time_START)))
-
-    else:
-
-        # Print Result Message
-        print(Fore.RED + "\nNice Try! Some Mistakes There Though, Wanna Do Another ?\n")
+    return textACC
 
 
-        print(Fore.GREEN + "[Accuracy:", TextAcc(plyr_text, displayText, word_count), "%]")
-        print(Fore.YELLOW + "[Time Taken:", TimeTaken(time_STOP, time_START), "s]")
-        print(Fore.MAGENTA + "[Words Per Minute:", 
-              WordsPerMinute(time_STOP, time_START, word_count, TimeTaken(time_STOP, time_START)),"]")
+
+def timeTaken(time_STOP, time_START):
+
+    # Gather Time Taken
+    timetaken = int(time_STOP - time_START)
+
+    return timetaken
 
 
-# RUNS APPLICATION
-os.system('clear') # clears terminal window
-generateChallengeText()
-plyrStart()
-checkPlyrScore()
+
+def wordsPerMinute(time_STOP, time_START, word_Count):
+
+    # Calculates Time Taken
+    timeTaken = time_STOP - time_START
+
+    # Calculates Words Per Minute
+    wordsPerMinute = round((word_Count / (time_STOP - time_START) * 100))
+
+    return wordsPerMinute
+
+
+
+#====================
+# Display Users Score
+#====================
+def displayUserScore():
+
+    try:
+
+        global plyr_text, displayText, time_STOP, time_START
+
+        # Checks If User Input Is Same As Printed Text
+        if plyr_text.strip() == displayText.strip() and plyr_text != "" and plyr_text.strip() >= displayText.strip():
+
+            textAccuracy = textAcc(plyr_text, displayText, len((displayText).split()))
+            timeTakenForTest = timeTaken(time_STOP, time_START)
+            wordsPerMin = wordsPerMinute(time_STOP, time_START, len((displayText).split()))
+
+
+            print(
+                Panel(
+                    f"[bold green]Accuracy: [/]{textAccuracy}%\n[bold yellow]Time Taken: [/]{timeTakenForTest}s\n[bold purple]Words Per Minute: [/]{wordsPerMin}",
+                    title="[bold italic green]You Did It ! Wanna Do Another ?[/]",
+                )
+            )
+
+        elif plyr_text.strip() != displayText.strip() and plyr_text != "" and plyr_text.strip() >= displayText.strip():
+
+            textAccuracy = textAcc(plyr_text, displayText, len((displayText).split()))
+            timeTakenForTest = timeTaken(time_STOP, time_START)
+            wordsPerMin = wordsPerMinute(time_STOP, time_START, len((displayText).split()))
+
+
+            print(
+                Panel(
+                    f"[bold green]Accuracy: [/]{textAccuracy}%\n[bold yellow]Time Taken: [/]{timeTakenForTest}s\n[bold purple]Words Per Minute: [/]{wordsPerMin}",
+                    title="[bold italic red]Nice Try! Some Mistakes There Though, Wanna Do Another ?[/]",
+                )
+            )
+
+        elif plyr_text.strip() < displayText.strip():
+
+            print(
+                Panel(
+                    "[bold red]Invalid test, text not fully typed.[/]",
+                    title="[bold italic red]ERROR[/]"
+                    )
+                )
+
+        else:
+
+             print(
+                Panel(
+                    "[bold red]Invalid Test, No Text Entered.[/]",
+                    title="[bold italic red]ERROR[/]"
+                    )
+                )
+
+    except:
+        pass
+
+
+
+if __name__ == "__main__":
+    os.system('clear') # clears terminal window
+    generateChallengeText()
+    displayChallengeText()
+    plyrStart()
+    displayUserScore()
 
