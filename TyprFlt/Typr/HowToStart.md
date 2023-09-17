cbb11390ee828bc4f84464977303d9e15bc7a977
Daksh <dakshkaul7790@gmail.com>
2023-09-17 16:02:36 +0530

    Formatted HTS file and slight unknown changes on main.py

---
+++ TyprFlt/Typr/HowToStart.md
@@ -0,0 +1,5 @@
+# To Create A Python Virtual Env
+python3 -m venv .venv
+
+# To Start Python Virtual Env
+source .venv/bin/activate
--- TyprFlt/Typr/main.py
+++ TyprFlt/Typr/main.py
@@ -51,73 +51,74 @@ def main(page: ft.Page):
     page.add(challengeText)
     page.update()
 
+    global usrIsTyping, timeStartState
     usrIsTyping = False
     timeStartState = False
 
-    def runtimeCheck(e):
-        global usrEntryBox, challengeText, usrIsTyping, timeStartState
+    def startTyping():
+        global usrIsTyping, timeStartState
+        if not usrIsTyping:
+            usrIsTyping = True
+            timeStartState = True
+            print("Time Started!")
 
+    def stopTyping(usrEntryBox, challengeText):
+        global usrIsTyping, timeStartState
         usrIsTyping = False
         timeStartState = False
+        timeSTOP = time.time()
+        print("Time Stopped!")
+
+        results = calculateResults(usrEntryBox, challengeText, timeSTOP)
+        displayResults(results)
+
+        resetInputs()
+
+    def calculateResults(usrEntryBox, challengeText, timeSTOP):
+        timeSTART = time.time()  # Define timeSTART here
+        acc = textAcc(usrEntryBox.value, challengeText.value, len(challengeText.value))
+        ttk = timeTaken(timeSTOP, timeSTART)
+        wpm = wordsPerMinute(ttk, len(challengeText.value))
+        results = list(wpm, acc, ttk)
+        return results
+
+    def displayResults(results):
+        resultRow = ft.Text(
+            f"RESULTS | WPM:{results[0]}, ACC:{results[1]}, TTK:{results[2]}"
+        )
+        page.show_snack_bar(ft.SnackBar(resultRow))
+        print("Results Calculated!")
+
+    def resetInputs():
+        usrEntryBox.value = ""
+        challengeText.value = str(stdfunc.conv_LTS(stdfunc.generateChallengeText(10)))
+        page.add(challengeText)
+        page.update()
+        print("New Text Generated!")
+
+    def onUserInput(e):
+        global usrEntryBox, challengeText
 
         if len(usrEntryBox.value) > 0:
-            if usrIsTyping is False:
-                usrIsTyping = True
+            if not usrIsTyping:
+                startTyping()
+            elif timeStartState and not usrIsTyping:
+                stopTyping(usrEntryBox, challengeText)
 
-            if usrIsTyping is True and timeStartState is False:
-                timeSTART = time.time()
-                timeStartState = True
+            if len(usrEntryBox.value) > len(challengeText.value):
+                usrEntryBox.error_text = "Incorrect. Try Again!"
+                resetInputs()
+                print("Too long :|")
 
-                print("Time Started!")
-
-            if timeStartState is True and usrIsTyping is False:
-                if len(usrEntryBox.value) == len(challengeText.value):
-                    timeSTOP = time.time()
-                    timeStartState = False
-
-                    print("Time Stoped!")
-
-                    results = [None] * 2
-
-                    results[1] = textAcc(
-                        usrEntryBox.value, challengeText.value, len(challengeText.value)
+            if len(usrEntryBox.value) == len(challengeText.value):
+                displayResults(
+                    calculateResults(
+                        usrEntryBox,
+                        challengeText,
                     )
-                    results[2] = timeTaken(timeSTOP, timeSTART)
-                    results[3] = wordsPerMinute(
-                        timeTaken(timeSTOP, timeSTART), len(challengeText.value)
-                    )
-
-                    resultRow = ft.Text(
-                        f"RESULTS | WPM:{results[2]}, ACC:{results[1]}, TTK:{results[0]}"
-                    )
-                    page.show_snack_bar(ft.SnackBar(resultRow))
-
-                    print("Results Calculated!")
-
-                    # Creating New Text For Usr
-                    usrEntryBox.value = ""
-                    challengeText.value = str(
-                        stdfunc.conv_LTS(stdfunc.generateChallengeText(10))
-                    )
-                    page.add(challengeText)
-                    page.update()
-
-                    print("New Text Generated!")
-
-                if len(usrEntryBox.value) > len(challengeText.value):
-                    usrEntryBox.error_text = "Incorrect. Try Again!"
-                    usrEntryBox.value = ""
-                    page.update()
-
-                    # Creating New Text For Usr
-                    usrEntryBox.value = ""
-                    challengeText.value = str(
-                        stdfunc.conv_LTS(stdfunc.generateChallengeText(10))
-                    )
-                    page.add(challengeText)
-                    page.update()
-
-                    print("Too long :|")
+                )
+                resetInputs()
+                print("Test Completed!")
 
     def textAcc(plyr_text, displayText, word_count):
         # Calculates Text Acc
@@ -140,9 +141,8 @@ def main(page: ft.Page):
         return wordsPerMinute
 
     global usrEntryBox
-    usrEntryBox = ft.TextField(label="Type the following text.", on_change=runtimeCheck)
+    usrEntryBox = ft.TextField(label="Type the following text.", on_change=onUserInput)
     page.add(usrEntryBox)
-    page.update()
 
     def checkANS_click(e):
         global userEntryBox, challengeText, usrIsTyping, timeTaken
