04e99ab74a06a301c5a78f6ff2364ab9321a7831
Daksh <dakshkaul7790@gmail.com>
2024-01-11 09:52:30 +0530

    Refactored Profile Page.

--- src/TyprFlt/Typr/pages/profile.py
+++ src/TyprFlt/Typr/pages/profile.py
@@ -7,25 +7,61 @@ class Profile(ft.UserControl):
     def __init__(self, page):
         super().__init__()
         self.page = page
+        self.initialize_page_settings()
+        self.create_page_controls()
 
-        page.title = "Typr: Profile"
+    def initialize_page_settings(self):
+        self.page.title = "Typr: Profile"
+        self.page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+        self.page.vertical_alignment = ft.MainAxisAlignment.CENTER
+        self.page.scroll = ft.ScrollMode.HIDDEN
+        self.page.on_resize = self.page_resize
 
-        page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-        page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    def create_page_controls(self):
+        self.pageHeader = self.create_page_header()
+        self.personalBestsBoard = self.create_personal_bests_board()
+        self.tests_data_file = px.data.gapminder().query("continent=='Oceania'")
+        self.wpm_figure, self.acc_figure, self.ttk_figure = self.create_figures()
+        self.profile_figure_container = self.create_profile_figure_container()
+        self.player_name_card = self.create_player_card("N/A", "N/A")
+        self.player_level_card = self.create_player_card("Typist Level", "Level: N/A")
+        self.time_played_card = self.create_player_card("Time Played", "Time: N/A")
+        self.return_btn = ft.ElevatedButton(
+            "Return", on_click=lambda _: self.page.go("/lessons")
+        )
+        self.logout_btn = ft.ElevatedButton(
+            "Logout", on_click=lambda _: self.page.go("/")
+        )
 
-        page.scroll = ft.ScrollMode.HIDDEN
+        self.pageContent = ft.ListView(
+            controls=[
+                self.pageHeader,
+                ft.Divider(thickness=10),
+                ft.Container(padding=20),
+                self.player_name_card,
+                ft.Container(padding=20),
+                self.player_level_card,
+                ft.Container(padding=20),
+                self.time_played_card,
+                ft.Container(padding=20),
+                ft.Container(
+                    self.personalBestsBoard, padding=ft.padding.only(bottom=20)
+                ),
+                ft.Container(padding=10),
+                self.profile_figure_container,
+                ft.Container(padding=20),
+                ft.Container(self.return_btn),
+                ft.Container(padding=10),
+                ft.Container(self.logout_btn),
+            ]
+        )
+        self.pageContent.alignment = ft.alignment.center
 
-        page.on_resize = self.page_resize
-
-        # ==============
-        # Page Controls
-        # ==============
-        self.pageHeader = ft.Row(
+    def create_page_header(self):
+        return ft.Row(
             controls=[
                 ft.Image(
-                    src="images/Astro_Typing.png",
-                    height=300,
-                    fit=ft.ImageFit.FIT_WIDTH,
+                    src="images/Astro_Typing.png", height=300, fit=ft.ImageFit.FIT_WIDTH
                 ),
                 ft.Container(
                     ft.Text(
@@ -45,7 +81,8 @@ class Profile(ft.UserControl):
             vertical_alignment=ft.CrossAxisAlignment.CENTER,
         )
 
-        self.personalBestsBoard = ft.DataTable(
+    def create_personal_bests_board(self):
+        return ft.DataTable(
             columns=[
                 ft.DataColumn(ft.Text("Words Per Minute"), numeric=True),
                 ft.DataColumn(ft.Text("Accuracy"), numeric=True),
@@ -57,34 +94,35 @@ class Profile(ft.UserControl):
                         ft.DataCell(ft.Text("N/A")),
                         ft.DataCell(ft.Text("N/A")),
                         ft.DataCell(ft.Text("N/A")),
-                    ],
+                    ]
                 ),
             ],
         )
 
-        self.testsDataFile = px.data.gapminder().query("continent=='Oceania'")
+    def create_figures(self):
+        wpm_figure = px.line(self.tests_data_file, x="pop", y="year", color="iso_num")
+        acc_figure = px.line(self.tests_data_file, x="pop", y="year", color="iso_num")
+        ttk_figure = px.line(self.tests_data_file, x="pop", y="year", color="iso_num")
+        return wpm_figure, acc_figure, ttk_figure
 
-        self.wpmFigure = px.line(self.testsDataFile, x="pop", y="year", color="iso_num")
-        self.accFigure = px.line(self.testsDataFile, x="pop", y="year", color="iso_num")
-        self.ttkFigure = px.line(self.testsDataFile, x="pop", y="year", color="iso_num")
-
-        self.profileFigureContainer = ft.Container(
+    def create_profile_figure_container(self):
+        return ft.Container(
             ft.Row(
                 controls=[
                     ft.Container(
-                        PlotlyChart(self.wpmFigure, expand=False, original_size=False),
+                        PlotlyChart(self.wpm_figure, expand=False, original_size=False),
                         width=800,
                         height=500,
                     ),
                     ft.Container(padding=10),
                     ft.Container(
-                        PlotlyChart(self.accFigure, expand=False, original_size=False),
+                        PlotlyChart(self.acc_figure, expand=False, original_size=False),
                         width=800,
                         height=500,
                     ),
                     ft.Container(padding=10),
                     ft.Container(
-                        PlotlyChart(self.ttkFigure, expand=False, original_size=False),
+                        PlotlyChart(self.ttk_figure, expand=False, original_size=False),
                         width=800,
                         height=500,
                     ),
@@ -92,13 +130,15 @@ class Profile(ft.UserControl):
             )
         )
 
-        self.playerNameCard = ft.Card(
+    def create_player_card(self, title, subtitle):
+        return ft.Card(
             content=ft.Container(
                 content=ft.Column(
                     [
                         ft.ListTile(
                             leading=ft.Icon(ft.icons.ALBUM),
-                            title=ft.Text("N/A", size=40),
+                            title=ft.Text(title, size=40),
+                            subtitle=ft.Text(subtitle, size=20),
                         ),
                     ]
                 ),
@@ -106,78 +146,6 @@ class Profile(ft.UserControl):
                 padding=10,
             )
         )
-
-        self.playerLevelCard = ft.Card(
-            content=ft.Container(
-                content=ft.Column(
-                    [
-                        ft.ListTile(
-                            leading=ft.Icon(ft.icons.ALBUM),
-                            title=ft.Text("Typist Level", size=40),
-                            subtitle=ft.Text(
-                                "Level: N/A",
-                                size=20,
-                            ),
-                        ),
-                    ]
-                ),
-                width=800,
-                padding=10,
-            )
-        )
-
-        self.timePlayedCard = ft.Card(
-            content=ft.Container(
-                content=ft.Column(
-                    [
-                        ft.ListTile(
-                            leading=ft.Icon(ft.icons.ALBUM),
-                            title=ft.Text("Time Played", size=40),
-                            subtitle=ft.Text(
-                                "Time: N/A",
-                                size=20,
-                            ),
-                        ),
-                    ]
-                ),
-                width=800,
-                padding=10,
-            )
-        )
-
-        self.returnBtn = ft.ElevatedButton(
-            "Return",
-            on_click=lambda _: self.page.go("/lessons"),
-        )
-
-        self.logoutBtn = ft.ElevatedButton(
-            "Logout",
-            on_click=lambda _: self.page.go("/"),
-        )
-
-        self.pageContent = ft.ListView(
-            controls=[
-                ft.Container(self.pageHeader),
-                ft.Divider(thickness=10),
-                ft.Container(padding=20),
-                ft.Container(self.playerNameCard),
-                ft.Container(padding=20),
-                ft.Container(self.playerLevelCard),
-                ft.Container(padding=20),
-                ft.Container(self.timePlayedCard),
-                ft.Container(padding=20),
-                ft.Container(
-                    self.personalBestsBoard, padding=ft.padding.only(bottom=20)
-                ),
-                ft.Container(padding=10),
-                ft.Container(self.profileFigureContainer),
-                ft.Container(padding=20),
-                ft.Container(self.returnBtn),
-                ft.Container(padding=10),
-                ft.Container(self.logoutBtn),
-            ],
-        )
-        self.pageContent.alignment = ft.alignment.center
 
     def page_resize(self, e):
         self.pageContent.alignment = ft.alignment.center
